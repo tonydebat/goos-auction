72934699b688b71170efb4ef523b66be986b36d4
Tony DeBat <tony.debat@gmail.com>
2010-03-09 13:36:03 -0500

    Initial commit.

---
+++ src/main/java/org/goos/auction/App.java
@@ -0,0 +1,13 @@
+package org.goos.auction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
