c6e99cb930ec4493c31a7f9ae98ec5c553fe3e95
Tony DeBat <tony.debat@gmail.com>
2010-04-04 16:10:29 -0400

    - CH11 pg 101 - Test is passing

---
+++ src/main/java/org/goos/auction/SingleMessageListener.java
@@ -0,0 +1,31 @@
+package org.goos.auction;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.hamcrest.Matcher;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+public class SingleMessageListener implements MessageListener {
+  private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<Message>(1);
+
+  public void processMessage(Chat chat, Message message) {
+    messages.add(message);
+  }
+
+  public void receivesAMessage() throws InterruptedException {
+    assertThat("Message", messages.poll(5, TimeUnit.SECONDS), is(notNullValue()));
+  }
+
+  public void receivesAMessage(Matcher<? super String> messageMatcher) throws InterruptedException {
+    final Message message = messages.poll(5, TimeUnit.SECONDS);
+    assertThat(message, hasProperty("body", messageMatcher));
+  }
+}
